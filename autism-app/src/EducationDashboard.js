75ec830376fe34b7f9b8e1cbf34b76e715b749c6
antoinevo <antoine.monarch@gmail.com>
2018-04-22 02:34:50 -0700

    Built out Graphs

--- autism-app/src/EducationDashboard.js
+++ autism-app/src/EducationDashboard.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import StudentsGallery from './StudentsGallery';
+import StudentPage from './StudentPage';
 
 const students = ["Joe", "Alice", "Fred", "Christina", "Joe", "Alex"]
 const teacher = ["Mr. Joe"]
 
 export default class EducationDashboard extends Component {
+    constructor(props){
+    super(props);
+    this.state = {
+      currentStudent: null,
+    };
+  }
   
-  
+  pickStudent = (id) => {
+		this.setState({ currentStudent: id });
+	}
 
+	renderStudentGallery = () => {
+		return this.state.currentStudent != null
+			? <StudentPage student={this.state.currentStudent}/>
+		  : <StudentsGallery students={students} teacher={teacher} pickStudent={this.pickStudent}/>
+	}
+	
 
   render() {
     return (
       <div className="educational-dashboard" >
-        <h1 class="title is-3 welcome">Welcome {teacher}</h1>
-     		<StudentsGallery students={students} />
+        {this.renderStudentGallery()}
       </div>
     );
   }
